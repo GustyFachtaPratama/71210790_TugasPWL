adde62e97bcd3afad61f667ca452e9966cf4f0d1
GustyFachtaPratama <112554244+GustyFachtaPratama@users.noreply.github.com>
2023-10-01 19:04:51 +0700

    Add files via upload

---
+++ Tugas kecil 2/env.js
@@ -0,0 +1,13 @@
+// 1. Membuat Env Variable sesuai data yang diregister
+const cat_env = {
+  API_KEY: "live_rd2tQA4OL5r7G4NDfBrVU2lmmCTiGtxBoRxqrPxNC2zfnAECx9nUyzqNNiDczNjv",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+const dog_env = {
+  API_KEY: "live_rd2tQA4OL5r7G4NDfBrVU2lmmCTiGtxBoRxqrPxNC2zfnAECx9nUyzqNNiDczNjv",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+// 2. Export dog_env
+export {dog_env };
